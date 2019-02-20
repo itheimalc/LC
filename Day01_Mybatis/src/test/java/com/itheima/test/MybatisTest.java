9305bab95a728d4b8086dcf703c7362a113b3c4c
admin <623231318@qq.com>
2019-02-20 08:49:47 +0800

    将Mybatis提交本地仓库

---
+++ Day01_Mybatis/src/test/java/com/itheima/test/MybatisTest.java
@@ -0,0 +1,36 @@
+package com.itheima.test;
+
+import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisTest {
+    /**
+     * 入门案例
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        //读取配置文件
+        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //创建SqlSessionFactory工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(is);
+        //使用工厂类生产SqlSession对象
+        SqlSession session = factory.openSession();
+        //使用SqlSession创建Dao接口的代理对象
+        UserDao userDao = session.getMapper(UserDao.class);
+        //使用代理对象执行方法
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        //释放资源`ssion.close();
+        is.close();
+    }
+}
